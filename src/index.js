76643093686e7fd83a1f5a2938664f0bd0d21df2
bengssxzz <bautistajr9999@yahoo.com>
2024-07-06 11:22:34 +0800

    Ready to deploy frontend

--- src/index.js
+++ src/index.js
@@ -10,7 +10,7 @@ import { BrowserRouter } from 'react-router-dom';
 
 import { disableReactDevTools } from '@fvilers/disable-react-devtools';
 
-if (process.env.NODE_ENV === 'production') disableReactDevTools();
+// if (process.env.NODE_ENV === 'production') disableReactDevTools();
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
